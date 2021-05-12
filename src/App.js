ce103abb4d6cfce02356365d758c9fbed33a5f77
joanne9989 <1619878217@qq.com>
2021-05-12 20:36:55 +0800

    got up to Adding A Task

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 
 function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+
   function addTask(name){
-    alert(name);
+    const newTask = { id: "id", name: name, completed: false };
+    setTasks([...tasks, newTask]); // ... means any number of args . CONTINUE FROM HERE      
   }
-  const taskList = props.tasks.map(task =>   
+  const taskList = tasks.map(task =>   
     <Todo 
       id={task.id} 
       name={task.name} 
--- src/components/Form.js
+++ src/components/Form.js
@@ -6,9 +6,17 @@ function Form(props) {
   // We are defining a function whose job is to modify name, called setName().
   // useState() returns these two things, so we are using array destructuring to capture them both in separate variables.
 
+  function handleChange(e){
+    setName(e.target.value); // e.target represents the element that fired the change event — that’s our input. So, value is the text inside it.
+  }
   function handleSubmit(e){
     e.preventDefault();
-    props.addTask("Say hello");
+    if (document.getElementById("new-todo-input").value !== "") {
+      props.addTask(name);
+      setName("");
+    } else {
+      alert("Field cannot be empty");
+    }
   }
     return (
         <form onSubmit = {handleSubmit}>
@@ -24,6 +32,7 @@ function Form(props) {
           name="text"
           autoComplete="off"
           value = {name}
+          onChange = {handleChange}
         />
         <button type="submit" className="btn btn__primary btn__lg">
           Add
